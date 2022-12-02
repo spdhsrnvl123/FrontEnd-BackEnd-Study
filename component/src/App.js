6143d8785de231ea5f2b173ca2e0d86242da75fe
이태형 <spdhsrnvl123@naver.com>
2022-12-02 20:39:32 +0900

    🎉React lecture start

---
+++ component/src/App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import MyComponent from './MyComponent';
+
+class App extends Component{
+  render(){
+    return (<MyComponent />)
+  }
+}
+
+export default App;
